e60c9ed6163c1fa886807e0cf43fee149b183504
Irem Kenar <64153000+iremkenar@users.noreply.github.com>
2020-06-30 12:20:02 +0200

    Added script file

---
+++ script.js
@@ -0,0 +1,18 @@
+magic8Ball = {};
+magic8Ball.listOfAnswers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes – definitely", "You may rely on it", "As I see it, yes", "Most likely","Outlook good" , "Yes", "Signs point to yes", "Reply hazy", "Try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful", "Not in 2020" ];
+// define the method
+magic8Ball.askQuestion = function(question) {
+     // create a random number
+     var randomNumber = Math.random();
+     // make the random number between 0 and the number of items in your array
+     var randomNumberForListOfAnswers = randomNumber * this.listOfAnswers.length;
+     // round down the number to be a whole number
+     var randomIndex = Math.floor(randomNumberForListOfAnswers);
+     // use that number to index a random number from the answers array
+     var answer = this.listOfAnswers[randomIndex];
+ 
+     console.log(question);
+     console.log(answer);
+};
+ 
+
